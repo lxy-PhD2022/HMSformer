e349695315ce90fb0b0002e1858a8c3b97b152b5
lxy <155620433+lxy-PhD2022@users.noreply.github.com>
2024-09-12 20:01:34 +0800

    Add files via upload

---
+++ scripts/MGSformer/exchange_rate.sh
@@ -0,0 +1,135 @@
+# add --individual for DLinear-I
+if [ ! -d "./logs" ]; then
+    mkdir ./logs
+fi
+
+if [ ! -d "./logs/LongForecasting" ]; then
+    mkdir ./logs/LongForecasting
+fi
+seq_len=96
+model_name=MGSformer
+model_id_name=Exchange
+
+for pred_len in 96
+do
+  python -u run_longExp.py \
+    --is_training 1 \
+    --root_path ./dataset/ \
+    --data_path exchange_rate.csv \
+    --model_id Exchange_$seq_len'_'$pred_len \
+    --model $model_name \
+    --data custom \
+    --features M \
+    --seq_len $seq_len \
+    --pred_len $pred_len \
+    --enc_in 8 \
+    --e_layers 1 \
+    --n_heads 1 \
+    --d_model 16 \
+    --d_ff 128 \
+    --dropout 0.3\
+    --fc_dropout 0.3\
+    --head_dropout 0\
+    --patch_len 16\
+    --stride 8\
+    --des 'Exp' \
+    --train_epochs 100\
+    --period 13\
+    --n 6\
+    --n_intra 12\
+    --compress_len 3\
+    --itr 1 --batch_size 128 --learning_rate 0.0001  >logs/LongForecasting/$model_name'_'$model_id_name'_'$seq_len'_'$pred_len.log
+done
+
+for pred_len in 192
+do
+  python -u run_longExp.py \
+    --is_training 1 \
+    --root_path ./dataset/ \
+    --data_path exchange_rate.csv \
+    --model_id Exchange_$seq_len'_'$pred_len \
+    --model $model_name \
+    --data custom \
+    --features M \
+    --seq_len $seq_len \
+    --pred_len $pred_len \
+    --enc_in 8 \
+    --e_layers 1 \
+    --n_heads 1 \
+    --d_model 16 \
+    --d_ff 128 \
+    --dropout 0.3\
+    --fc_dropout 0.3\
+    --head_dropout 0\
+    --patch_len 16\
+    --stride 8\
+    --des 'Exp' \
+    --train_epochs 100\
+    --period 6\
+    --n 10\
+    --n_intra 4\
+    --compress_len 3\
+    --itr 1 --batch_size 128 --learning_rate 0.0001  >logs/LongForecasting/$model_name'_'$model_id_name'_'$seq_len'_'$pred_len.log
+done
+
+for pred_len in 336
+do
+  python -u run_longExp.py \
+    --is_training 1 \
+    --root_path ./dataset/ \
+    --data_path exchange_rate.csv \
+    --model_id Exchange_$seq_len'_'$pred_len \
+    --model $model_name \
+    --data custom \
+    --features M \
+    --seq_len $seq_len \
+    --pred_len $pred_len \
+    --enc_in 8 \
+    --e_layers 1 \
+    --n_heads 1 \
+    --d_model 16 \
+    --d_ff 128 \
+    --dropout 0.3\
+    --fc_dropout 0.3\
+    --head_dropout 0\
+    --patch_len 16\
+    --stride 8\
+    --des 'Exp' \
+    --train_epochs 100\
+    --period 7\
+    --n 12\
+    --n_intra 1\
+    --compress_len 1\
+    --itr 1 --batch_size 128 --learning_rate 0.0001  >logs/LongForecasting/$model_name'_'$model_id_name'_'$seq_len'_'$pred_len.log
+done
+
+for pred_len in 720
+do
+  python -u run_longExp.py \
+    --is_training 1 \
+    --root_path ./dataset/ \
+    --data_path exchange_rate.csv \
+    --model_id Exchange_$seq_len'_'$pred_len \
+    --model $model_name \
+    --data custom \
+    --features M \
+    --seq_len $seq_len \
+    --pred_len $pred_len \
+    --enc_in 8 \
+    --e_layers 1 \
+    --n_heads 1 \
+    --d_model 16 \
+    --d_ff 128 \
+    --dropout 0.3\
+    --fc_dropout 0.3\
+    --head_dropout 0\
+    --patch_len 16\
+    --stride 8\
+    --des 'Exp' \
+    --train_epochs 100\
+    --period 24\
+    --n 1\
+    --n_intra 1\
+    --compress_len 10\
+    --itr 1 --batch_size 32 --learning_rate 0.0001  >logs/LongForecasting/$model_name'_'$model_id_name'_'$seq_len'_'$pred_len.log
+done
